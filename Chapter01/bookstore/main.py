2b6d16c0dca0fa9175a37873377d939b85734850
jeanpool.garcia.contractor <jeanpool.garcia.contractor@bbva.com>
2025-04-12 22:50:10 -0600

    feat: customizd data validation errors response

--- Chapter01/bookstore/main.py
+++ Chapter01/bookstore/main.py
@@ -1,6 +1,10 @@
 import router
 
-from fastapi import FastAPI, HTTPException
+import json
+
+from fastapi import FastAPI, HTTPException, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
 from starlette.responses import JSONResponse
 
 app = FastAPI()
@@ -24,6 +28,16 @@ async def http_exception_handler(request, exc):
         }
     )
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+    request: Request,
+    exc: RequestValidationError,
+):
+    return PlainTextResponse(
+        "This is a plain text response: " +
+        f"\n{json.dumps(exc.errors(), indent=2)}",
+        status_code= status.HTTP_400_BAD_REQUEST,
+    )
 # @app.get("/authors/{author_id}")
 # async def read_author(author_id: int):
 #     return {
