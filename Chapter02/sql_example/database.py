218652c5995f65233c9ea250865410688ba195b0
jeanpool.garcia.contractor <jeanpool.garcia.contractor@bbva.com>
2025-04-29 23:00:51 -0600

    feat: define User model and define its columns as class attributes

--- Chapter02/sql_example/database.py
+++ Chapter02/sql_example/database.py
@@ -1,5 +1,20 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    DeclarativeBase
+)
 
 
 class Base(DeclarativeBase):
+    """
+        Class to mantains a catalog of classes and tables
+    """
     pass
+
+class User(Base):
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(
+        primary_key=True
+    )
+    name: Mapped[str]
+    email: Mapped[str]
