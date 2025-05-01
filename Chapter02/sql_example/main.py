8e09fd77c0338c080171565587314a35075357c9
jeanpool.garcia.contractor <jeanpool.garcia.contractor@bbva.com>
2025-04-30 21:01:04 -0600

    add endpoint to get Users info

--- Chapter02/sql_example/main.py
+++ Chapter02/sql_example/main.py
@@ -1,4 +1,7 @@
-from database import SessionLocal
+from fastapi import Depends, FastAPI
+
+from sqlalchemy.orm import Session
+from database import SessionLocal, User
 
 def get_db():
     db = SessionLocal()
@@ -6,3 +9,10 @@ def get_db():
         yield db
     finally:
         db.close()
+
+app = FastAPI()
+
+@app.get("/users/")
+def read_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
