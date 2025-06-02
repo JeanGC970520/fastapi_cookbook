455e91f2b8b549cdf216958c79616e3159e49937
Jean Pool GC <jeanpoolgarcia_97@hotmail.com>
2025-06-01 21:23:24 -0600

    docs: add docstring in every endpoint

--- Chapter02/uploads_and_downloads/main.py
+++ Chapter02/uploads_and_downloads/main.py
@@ -12,6 +12,12 @@ app = FastAPI()
 async def upload_file(
     file: UploadFile = File(...),  # Suspensive dots -> Field(...) meaning that is required
 ):
+    """ Endpoint to upload a file into the server
+    Args:
+        file: required file to save it
+    Headers:
+        Content-Type: must be equal to multipart/form-data
+    """
     dir_path = os.path.realpath(__file__).replace("main.py", "") # Using to get root dir of main.py module
     with open(f"{dir_path}/uploads/{file.filename}", "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
@@ -22,6 +28,11 @@ async def upload_file(
     response_class= FileResponse,
 )
 async def dowload_file(filename: str):
+    """ Endpoint to download file, FastAPI has a FileResponse to help us
+    with this task. It streams files from the server
+    Args:
+        filename: name of file to get its content
+    """
     dir_path = os.path.realpath(__file__).replace("main.py", "")
     if not Path(f"{dir_path}/uploads/{filename}").exists():
         raise HTTPException(
