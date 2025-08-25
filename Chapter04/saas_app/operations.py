14a7f4f796a395657e82ee3cb705e3dc8268fd3f
Jean Pool GC <jeanpoolgarcia_97@hotmail.com>
2025-08-24 20:42:09 -0600

    feat: add method to get a valid user from User table

--- Chapter04/saas_app/operations.py
+++ Chapter04/saas_app/operations.py
@@ -1,3 +1,7 @@
+from email_validator import (
+    EmailNotValidError,
+    validate_email,
+)
 from passlib.context import CryptContext
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
@@ -30,3 +34,19 @@ def add_user(
         session.rollback()
         return
     return db_user
+
+
+def get_user(
+    session: Session, username_or_email: str
+) -> User | None:
+    try:
+        validate_email(username_or_email)
+        query_filter = User.email
+    except EmailNotValidError:
+        query_filter = User.username
+    user = (
+        session.query(User)
+        .filter(query_filter == username_or_email)
+        .first()
+    )
+    return user
